dc9010e2582707c1f9323cbb7957791388a36faa
max-chauvineau <chauvineaumaxime@yahoo.fr>
2025-02-14 09:49:18 +0100

    extract_light

--- R/MW8light.R
+++ R/MW8light.R
@@ -27,55 +27,60 @@ extract_light <- function(file) {
     )
   )
   original_light <- as.numeric(unlist(strsplit(light_text, ",")))
-
-  # Créer la série temporelle initiale basée sur les données d'origine
   original_length <- length(original_light)
-  start_date <- extract_start_recording(file)
-  epoch <- extract_frequency_light(file)
-  light <- data.frame(
-    time = seq(from = start_date, by = epoch, length.out = length(original_light)),
-    light = original_light
-  )
 
-  # Trouver les sections de changement (modifications manuelles)
-  change_nodes <- xml2::xml_find_all(xml2::read_xml(file), "//change[channel[@edit='true']][channel/name='motion']")
+  if (original_length > 1) {
+    # Créer la série temporelle initiale basée sur les données d'origine
+    start_date <- extract_start_recording(file)
+    epoch <- extract_frequency_light(file)
+    light <- data.frame(
+      time = seq(from = start_date, by = epoch, length.out = length(original_light)),
+      light = original_light
+    )
 
-  # Si des modifications existent, traiter chaque changement
-  if (length(change_nodes) > 0) {
-    for (change_node in change_nodes) {
-      # Extraire offset et epoch modifié pour le changement
-      offset <- as.numeric(xml2::xml_text(xml2::xml_find_first(change_node, ".//offset")))
-      modified_epoch <- as.numeric(xml2::xml_text(xml2::xml_find_first(change_node, ".//epoch")))
+    # Trouver les sections de changement (modifications manuelles)
+    change_nodes <- xml2::xml_find_all(xml2::read_xml(file), "//change[channel[@edit='true']][channel/name='motion']")
 
-      # Extraire les nouvelles données de mouvement pour ce changement
-      modified_data_text <- xml2::xml_text(xml2::xml_find_first(change_node, ".//data"))
-      modified_data <- as.numeric(unlist(strsplit(modified_data_text, ",")))
+    # Si des modifications existent, traiter chaque changement
+    if (length(change_nodes) > 0) {
+      for (change_node in change_nodes) {
+        # Extraire offset et epoch modifié pour le changement
+        offset <- as.numeric(xml2::xml_text(xml2::xml_find_first(change_node, ".//offset")))
+        modified_epoch <- as.numeric(xml2::xml_text(xml2::xml_find_first(change_node, ".//epoch")))
 
-      # Calculer l'index de début dans motion basé sur l'offset (en secondes)
-      start_index <- offset / epoch + 1
-      end_index <- start_index + length(modified_data) - 1
+        # Extraire les nouvelles données de mouvement pour ce changement
+        modified_data_text <- xml2::xml_text(xml2::xml_find_first(change_node, ".//data"))
+        modified_data <- as.numeric(unlist(strsplit(modified_data_text, ",")))
 
-      # Si light est trop courte, l'étendre
-      if (end_index > nrow(light)) {
-        additional_rows <- data.frame(
-          time = seq(
-            from = light$time[nrow(light)] + epoch,
-            by = epoch,
-            length.out = end_index - nrow(light)
-          ),
-          light = NA
-        )
-        light <- rbind(light, additional_rows)
+        # Calculer l'index de début dans motion basé sur l'offset (en secondes)
+        start_index <- offset / epoch + 1
+        end_index <- start_index + length(modified_data) - 1
+
+        # Si light est trop courte, l'étendre
+        if (end_index > nrow(light)) {
+          additional_rows <- data.frame(
+            time = seq(
+              from = light$time[nrow(light)] + epoch,
+              by = epoch,
+              length.out = end_index - nrow(light)
+            ),
+            light = NA
+          )
+          light <- rbind(light, additional_rows)
+        }
+
+        light$light[start_index:end_index] <- modified_data
       }
-
-      # Remplacer les valeurs d'origine par les nouvelles données
-      light$light[start_index:end_index] <- modified_data
     }
+
+    light$light[light$light == -1] <- NA
+    light <- light %>%
+      dplyr::filter(dplyr::row_number() >= min(which(!is.na(light))) & dplyr::row_number() <= max(which(!is.na(light))))
+  } else {
+    light <- as.data.frame(list(
+      time = numeric(0),
+      light = numeric(0)
+    ))
   }
-
-  light$light[light$light == -1] <- NA
-  light <- light %>%
-    dplyr::filter(dplyr::row_number() >= min(which(!is.na(light))) & dplyr::row_number() <= max(which(!is.na(light))))
-
   return(light)
 }
